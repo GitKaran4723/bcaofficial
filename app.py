c81edb9f9d684cdab6b391bed1f7509c17b4c54b
Karan <Karansj4723@gmail.com>
2025-07-07 07:10:05 +0530

    Added table of applicants

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+GOOGLE_SCRIPT_URL = os.getenv('GOOGLE_SCRIPT_URL')
+
+@app.route('/')
+def display_data():
+    try:
+        response = requests.get(GOOGLE_SCRIPT_URL)
+        data = response.json()
+    except Exception as e:
+        return f"Error fetching data: {e}"
+
+    return render_template("gfApp.html", data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
